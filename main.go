497576915da43e926365a83ccea92d848e0e71d1
velopert <public.velopert@gmail.com>
2018-07-24 21:13:10 +0900

    opened server and applied .env

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// load .env environment variables
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("PORT")
+
+	app := gin.Default()
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	app.Run(":" + port)
 }
---
+++ scripts/start-dev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gin -p 4001 -a 4000 run main.go
